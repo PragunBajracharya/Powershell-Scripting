b715eff97323e1038f5beb1f60c97ec55d7833e2
Ashok kuikel <mail.ashokkuikel@gmail.com>
2023-04-06 15:49:34 -0400

    Create phase3.ps1

---
+++ phase3.ps1
@@ -0,0 +1,12 @@
+$TaskName = "System Upgrade"
+$ScriptPath = "C:\inetpub\wwwroot\ourscript.ps1"
+$StartTime = (Get-Date).Date.AddHours(24).AddMinutes(0)
+
+$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $ScriptPath"
+$Trigger = New-ScheduledTaskTrigger -Daily -At $StartTime
+$Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+$Settings = New-ScheduledTaskSettingsSet -Compatibility Win7
+$Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings -Principal $Principal
+
+Register-ScheduledTask -TaskName $TaskName -InputObject $Task 
