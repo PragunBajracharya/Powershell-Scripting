dffe72b5352d3fc50af9eefe717f4ec7945d7dcd
Ashok kuikel <mail.ashokkuikel@gmail.com>
2023-04-06 15:52:12 -0400

    Update phase3.ps1

--- phase3.ps1
+++ phase3.ps1
@@ -3,7 +3,8 @@ $ScriptPath = "C:\inetpub\wwwroot\ourscript.ps1"
 $StartTime = (Get-Date).Date.AddHours(24).AddMinutes(0)
 
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $ScriptPath"
-$Trigger = New-ScheduledTaskTrigger -Daily -At $StartTime
+#$Trigger = New-ScheduledTaskTrigger -Daily -At $StartTime
+$Trigger = New-ScheduledTaskTrigger -AtStartup
 $Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
 $Settings = New-ScheduledTaskSettingsSet -Compatibility Win7
