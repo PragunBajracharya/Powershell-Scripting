54fe7f5ea44a1129ca30a9147721925535d024f9
Pragun Bajracharya <pragunbaj99@gmail.com>
2023-04-05 23:11:03 -0400

    Update phase2.ps1

--- phase2.ps1
+++ phase2.ps1
@@ -3,8 +3,8 @@
 #removeLogFilesFromAFolder -Folderpath "C:\Windows\System32\winevt\Logs"
 #removeLogFilesFromAFolder -Folderpath "C:\inetpub\logs\LogFiles"
 
-Remove-LocalGroupMember -Group "Administrators" -Member "Administrator"
-Set-LocalUser -Name "Administrator" -UserMayChangePassword $false
+New-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString "p@ssword123" -AsPlainText -Force) -FullName "Administrator" -Description ""
+Add-LocalGroupMember -Group "Guests" -Member "Administrator"
 
 # Enable Remote Desktop Connection
 Set-ItemProperty ‘HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\‘ -Name “fDenyTSConnections” -Value 0
