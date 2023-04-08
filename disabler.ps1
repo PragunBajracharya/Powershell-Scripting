cbb80b2c6a39848758038365e89c8d3646924ff1
Pragun Bajracharya <pragunbaj99@gmail.com>
2023-04-07 23:00:43 -0400

    Enabled firewall

--- disabler.ps1
+++ disabler.ps1
@@ -4,3 +4,5 @@ net user Administator /active:no
 #Disabled the EventLog Service
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WMI\Autologger\EventLog-Application" -Name "Start" -Value 1 -Type DWord
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\Application" -Name "Start" -Value 2 -Type DWord
+
+Set-NetFirewallProfile -Enabled True
